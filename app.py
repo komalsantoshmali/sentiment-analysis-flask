96151220a6593ca83b15d80f2b56e8348c527dcb
komalsantoshmali <km7323158@gmail.com>
2026-02-05 17:50:42 +0530

    Initial commit: Flask Sentiment Analysis App

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+from textblob import TextBlob
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    sentiment = ""
+    polarity = 0
+    subjectivity = 0
+
+    if request.method == "POST":
+        text = request.form["text"]
+        blob = TextBlob(text)
+
+        polarity = blob.sentiment.polarity
+        subjectivity = blob.sentiment.subjectivity
+
+        if polarity > 0:
+            sentiment = "Positive 😊"
+        elif polarity < 0:
+            sentiment = "Negative 😠"
+        else:
+            sentiment = "Neutral 😐"
+
+    return render_template(
+        "index.html",
+        sentiment=sentiment,
+        polarity=polarity,
+        subjectivity=subjectivity
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
